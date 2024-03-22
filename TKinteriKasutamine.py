ee304df2779b458eb5ecf0eb8d6085ddb8a7e0a5
RipMyMartin <161806791+RipMyMartin@users.noreply.github.com>
2024-03-22 11:34:13 +0200

    Add files via upload

---
+++ TKinteriKasutamine.py
@@ -0,0 +1,73 @@
+﻿from tkinter import *
+
+k=0
+def vajuta():
+    global k
+    k+=1
+    nupp.config(text=k)
+
+def vajuta_(event):
+    global k
+    k-=1
+    nupp.config(text=k)
+
+def textboxtist(event):
+    t=textbox.get()
+    pealkiri.configure(text=t,width=len(t))
+    textbox.delete(0,END)
+
+def valik():
+    arv=var.get()
+    textbox.incert(END,arv)
+
+
+
+aken = Tk()
+aken.geometry("400x200")
+aken.iconbitmap('martinsild.ico')
+aken.title("TK_Kasutamine")
+tekst= "Pealkiri\n"
+pealkiri = Label(aken,
+                 text=tekst,
+                 bg="#ff7373",
+                 fg="#ff7373",
+                 font="Algerian 20",
+                 height= 3,width=len(tekst),
+                 cursor=("man"))
+textbox=Entry(aken,
+              bg="#ff7373",
+              fg="#ff7373",
+              font="Algerian 20",
+              width=20,
+              justify=CENTER) #show="****" отоброжать будет *****
+nupp=Button(aken,
+            text="Vajuta mind!",
+            font="Arial 20",
+            height=3,width=10,
+            relief=RAISED,
+            bg="lightblue",
+            command=vajuta,
+            )#SUNKEN, GROOVE
+
+f=Frame(aken)
+var=IntVar() #StringVar()
+e_=Radiobutton(f,text="Esimene",variable=var,value=1,command=valik)
+t_=Radiobutton(f,text="Teine",variable=var,value=2,command=valik)
+k_=Radiobutton(f,text="Kolmas",variable=var,value=3,command=valik)
+
+
+nupp.bind("<Button-3>",vajuta_)
+textbox.bind("<Return>",textboxtist)#Enter
+
+obj=[pealkiri,textbox,nupp,f]
+for i in obj:
+    i.pack()
+
+obj2=[e_,t_,k_]
+for n in range(len(obj2)):
+    obj2[n].grid(row=0,column=1)
+
+pealkiri.pack()
+textbox.pack()
+nupp.pack()
+aken.mainloop()
---
+++ ruutvorrandilahendamine.py
@@ -0,0 +1,61 @@
+﻿from tkinter import *
+
+k=0
+def vajuta():
+    global k
+    k+=1
+    nupp.config(text=k)
+
+def vajuta_(event):
+    global k
+    k-=1
+    nupp.config(text=k)
+
+#def textboxtist(event):
+#    t=textbox.get()
+#    pealkiri.configure(text=t,width=len(t))
+#    textbox.delete(0,END)
+
+def valik():
+    arv=var.get()
+    textbox.incert(END,arv)
+
+
+
+aken = Tk()
+aken.configure(bg='yellow')
+aken.geometry("700x250")
+aken.iconbitmap('martinsild.ico')
+aken.title("TK_Kasutamine")
+tekst= "Решение квадратного уровнение\n"
+
+pealkiri = Label(aken,
+                 text=tekst,
+                 bg="#96f3eb",
+                 fg="#3ba243",
+                 font="Algerian 16",
+                 height=2,width=len(tekst),
+                 cursor=("man"))
+textbox=Entry(aken,
+              bg="#000000",
+              fg="#ff7373",
+              font="Algerian 20",
+              justify=LEFT,
+              width=20,
+              ) #show="****" отоброжать будет *****
+
+nupp=Button(aken,
+            text="Vajuta mind!",
+            font="Arial 20",
+            height=3,width=10,
+            relief=RAISED,
+            bg="lightblue",
+            command=vajuta,
+            )#SUNKEN, GROOVE
+
+
+obj=[pealkiri,textbox,nupp,f]
+for i in obj:
+    i.pack()
+
+aken.mainloop()
