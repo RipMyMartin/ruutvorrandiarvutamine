18a5b0dcab2e135dcc4adcb8d8952894931ceb9d
RipMyMartin <161806791+RipMyMartin@users.noreply.github.com>
2024-04-12 14:56:12 +0300

    Update ruutMartinSildTARpv23.py

--- ruutMartinSildTARpv23.py
+++ ruutMartinSildTARpv23.py
@@ -133,6 +133,50 @@ def liblikas():
     plt.grid(True)
     plt.show()
 
+
+
+def liblikas():
+    x1=np.arange(-9,-1.5,0.5)
+    y1=(-1/8)*(x1+9)**2+8
+    x2=np.arange(1,9.5,0.5)
+    y2=(-1/8)*(x2-9)**2+8
+    x3=np.arange(-9,8.5,0.5)
+    y3=7*(x3+8)**2+1
+    x4=np.arange(8,9.5,0.5)
+    y4=7*(x4-8)**2-1
+    x5=np.arange(-8,-1.5,0.5)
+    y5=(1/49)*(x5+1)**2
+    x6=np.arange(1,8.5,0.5)
+    y6=(1/49)*(x6-1)**2
+    x7=np.arange(-8,-1.5,0.5)
+    y7=(-4/49)*(x7+1)**2
+    x8=np.arange(1,8.5,0.5)
+    y8=(-4/49)*(x8-1)**2
+    x9=np.arange(-8,-2.5,0.5)
+    y9=(1/3)*(x9+5)**2-7
+    x10=np.arange(2,8.5,0.5)
+    y10=(1/3)*(x10-5)**2-7
+    x11=np.arange(-2,1.5,0.5)
+    y11=-2*(x11+1)**2-2
+    x12=np.arange(1,2.5,0.5)
+    y12=-2*(x12-1)**2-2
+    x13=np.arange(-1,1.5,0.5)
+    y13=-4*x13**2+2
+    x14=np.arange(-1,1.5,0.5)
+    y14=4*x14**2-6
+    x15=np.arange(-2,0.5,0.5)
+    y15=-1,5*x15+2
+    x16=np.arange(0.5,2.5,0.5)
+    y16=1.5*x16+2
+    fig =plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10,x11,y11,x12,y12,x13,y13,x14,y14,x15,y15,x16,y16)
+    plt.title('Бабочка')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
+
+
     
 
 root = Tk()
