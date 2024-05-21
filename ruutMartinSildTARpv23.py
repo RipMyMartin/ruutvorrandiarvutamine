74a87b7bb565d16a66ad8c23b8fc11be935083eb
RipMyMartin <161806791+RipMyMartin@users.noreply.github.com>
2024-05-21 18:20:45 +0300

    Update ruutMartinSildTARpv23.py

--- ruutMartinSildTARpv23.py
+++ ruutMartinSildTARpv23.py
@@ -1,4 +1,4 @@
-﻿from tkinter import *
+from tkinter import *
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -46,7 +46,7 @@ def graafik():
         plt.xlabel('x')
         plt.grid(True)
         plt.show()
-        text = f"Вершина параболлы: ({x0:.2f}, {y0:.2f})"
+        text = f"Вершина параболы: ({x0:.2f}, {y0:.2f})"
     except ValueError:
         text = "График невозможно построить из-за ошибки ввода."
     solution_label.config(text=text)
@@ -63,6 +63,89 @@ def increase_window_height():
         button_increase_height.config(text="Уменьшить окно")
         for radio in radios:
             radio.grid()
+
+
+
+def kala():
+    x1 = np.arange(0, 9.5, 0.5)
+    x2 = np.arange(-10, 0.5, 0.5)
+    x3 = np.arange(-9, -2.5, 0.5)
+    x4 = np.arange(-3, 9.5, 0.5)
+    x5 = np.arange(5, 9, 0.5)
+    x6 = np.arange(5, 8.5, 0.5)
+    x7 = np.arange(-13, -8.5, 0.5)
+    x8 = np.arange(-15, -12.5, 0.5)
+    x9 = np.arange(-15, -10, 0.5)
+    x10 = np.arange(3,4,0.5)
+
+    y1= (2/27)*x1*x1-3
+    y2= 0.04*x2*x2-3
+    y3= (2/9)*(x3+6)**2+1
+    y4= (-1/12)*(x4-3)**2+6
+    y5= (1/9)*(x5-5)**2+2
+    y6= (1/8)*(x6-7)**2+1.5
+    y7= -0.75*(x7+11)**2+6
+    y8= (-0.5)*(x8+13)**2+3
+    y9= [1]*len(x9)
+    y10= [3]*len(x10)
+
+    plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10)
+    plt.title("Kala")
+    plt.ylabel("y")
+    plt.xlabel("x")
+    plt.grid(True)
+    plt.show()
+
+
+def prillid():
+    x1 = np.arange(-9, -0.5, 0.5)
+    x2 = np.arange(1, 9.5, 0.5)
+    x3 = np.arange(-9, -0.5, 0.5)
+    x4 = np.arange(1, 9.5, 0.5)
+    x5 = np.arange(-9, -5.5, 0.5)
+    x6 = np.arange(6, 9.5, 0.5)
+    x7 = np.arange(-1, 1.5, 0.5)
+
+    y1= (-1/16)*(x1+5)**2+2
+    y2= (-1/16)*(x2-5)**2+2
+    y3= (1/4)*(x3+5)**2-3
+    y4= (1/4)*(x4-5)**2-3
+    y5= -1*(x5+7)**2+5
+    y6= -1*(x6-7)**2+5
+    y7= -0.5*x7**2+1.5
+
+    plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7)
+    plt.title("Prillid")
+    plt.ylabel("y")
+    plt.xlabel("x")
+    plt.grid(True)
+    plt.show()
+
+def vihmavari():
+    x1 = np.arange(-12, 12.5, 0.5)
+    x2 = np.arange(-4, 4.5, 0.5)
+    x3 = np.arange(-12, -3.5, 0.5)
+    x4 = np.arange(4, 12.5, 0.5)
+    x5 = np.arange(-4, 0.5, 0.5)
+    x6 = np.arange(-4, 0.7, 0.5)
+
+    y1= (-1/18)*x1**2+12
+    y2= (-1/8)*x2**2+6
+    y3= (-1/8)*(x3+8)**2+6
+    y4= (-1/8)*(x4-8)**2+6
+    y5= 2*(x5+3)**2-9
+    y6= 1.5*(x6+3)**2-10
+
+    plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6)
+    plt.title("Vihmavari")
+    plt.ylabel("y")
+    plt.xlabel("x")
+    plt.grid(True)
+    plt.show()
+
 
 def vaal():
     x1 = np.arange(0, 9.5, 0.5)
@@ -86,22 +169,21 @@ def vaal():
     x10 = np.arange(3, 4, 0.5)
     y10 = [3] * len(x10)
     plt.plot(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10)
-    plt.title('Кит')
+    plt.title('Vaal')
     plt.ylabel('y')
     plt.xlabel('x')
     plt.grid(True)
     plt.show()
 
-
-def liblikas():
+'''def liblikas():
     x1 = np.arange(-9, -1.5, 0.5)
-    y1 = (-1/8) * (x1 - 9) ** 2 + 8
+    y1 = (-1/8) * (x1 + 9) ** 2 + 8
     x2 = np.arange(1, 9.5, 0.5)
     y2 = (-1/8) * (x2 - 9) ** 2 + 8
     x3 = np.arange(-9, 8.5, 0.5)
     y3 = 7 * (x3 + 8) ** 2 + 1
     x4 = np.arange(8, 9.5, 0.5)
-    y4 = 7 * (x4 + 8) ** 2 + 1
+    y4 = 7 * (x4 - 8) ** 2 - 1
     x5 = np.arange(-8, -1.5, 0.5)
     y5 = (1/49) * (x5 + 1) ** 2
     x6 = np.arange(1, 8.5, 0.5)
@@ -109,7 +191,7 @@ def liblikas():
     x7 = np.arange(-8, -1.5, 0.5)
     y7 = (-4/49) * (x7 + 1) ** 2
     x8 = np.arange(1, 8.5, 0.5)
-    y8 = (-4/49) * (x8 + 1) ** 2
+    y8 = (-4/49) * (x8 - 1) ** 2
     x9 = np.arange(-8, -2.5, 0.5)
     y9 = (1/3) * (x9 + 5) ** 2 - 7
     x10 = np.arange(2, 8.5, 0.5)
@@ -126,62 +208,18 @@ def liblikas():
     y15 = -1.5 * x15 + 2
     x16 = np.arange(0.5, 2.5, 0.5)
     y16 = 1.5 * x16 + 2
+    fig = plt.figure()
     plt.plot(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10, x11, y11, x12, y12, x13, y13, x14, y14, x15, y15, x16, y16)
-    plt.title('Бабочка')
+    plt.title('Liblikas')
     plt.ylabel('y')
     plt.xlabel('x')
     plt.grid(True)
     plt.show()
 
-
-
-def liblikas():
-    x1=np.arange(-9,-1.5,0.5)
-    y1=(-1/8)*(x1+9)**2+8
-    x2=np.arange(1,9.5,0.5)
-    y2=(-1/8)*(x2-9)**2+8
-    x3=np.arange(-9,8.5,0.5)
-    y3=7*(x3+8)**2+1
-    x4=np.arange(8,9.5,0.5)
-    y4=7*(x4-8)**2-1
-    x5=np.arange(-8,-1.5,0.5)
-    y5=(1/49)*(x5+1)**2
-    x6=np.arange(1,8.5,0.5)
-    y6=(1/49)*(x6-1)**2
-    x7=np.arange(-8,-1.5,0.5)
-    y7=(-4/49)*(x7+1)**2
-    x8=np.arange(1,8.5,0.5)
-    y8=(-4/49)*(x8-1)**2
-    x9=np.arange(-8,-2.5,0.5)
-    y9=(1/3)*(x9+5)**2-7
-    x10=np.arange(2,8.5,0.5)
-    y10=(1/3)*(x10-5)**2-7
-    x11=np.arange(-2,1.5,0.5)
-    y11=-2*(x11+1)**2-2
-    x12=np.arange(1,2.5,0.5)
-    y12=-2*(x12-1)**2-2
-    x13=np.arange(-1,1.5,0.5)
-    y13=-4*x13**2+2
-    x14=np.arange(-1,1.5,0.5)
-    y14=4*x14**2-6
-    x15=np.arange(-2,0.5,0.5)
-    y15=-1,5*x15+2
-    x16=np.arange(0.5,2.5,0.5)
-    y16=1.5*x16+2
-    fig =plt.figure()
-    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10,x11,y11,x12,y12,x13,y13,x14,y14,x15,y15,x16,y16)
-    plt.title('Бабочка')
-    plt.ylabel('y')
-    plt.xlabel('x')
-    plt.grid(True)
-    plt.show()
-
-
-    
+    '''
 
 root = Tk()
 root.geometry("1100x350")
-root.iconbitmap('martinsild.ico')
 root.title("TK_Inter usage")
 
 title_label = Label(root,
@@ -269,14 +307,28 @@ b_.bind("<KeyRelease>", entry_changed)
 c_.bind("<KeyRelease>", entry_changed)
 
 radios = []
-for i, text in enumerate(["Кит"]):
+for i, text in enumerate(["Vaal"]):
     radio = Radiobutton(button_frame, text=text, command=vaal)
-    radio.grid(row=5, column=4, padx=10, pady=10)
+    radio.grid(row=5, column=1, padx=10, pady=10)
     radios.append(radio)
 
-for i, text in enumerate(["Бабочка"]):
+for i, text in enumerate(["Liblikas"]):
     radio = Radiobutton(button_frame, text=text, command=liblikas)
-    radio.grid(row=8, column=1, padx=10, pady=10)
+    radio.grid(row=5, column=2, padx=10, pady=10)
     radios.append(radio)
+
+for i, text in enumerate(["Vihmavari"]):
+    radio = Radiobutton(button_frame, text=text, command=vihmavari)
+    radio.grid(row=5, column=2, padx=10, pady=10)
+    radios.append(radio)
+
+
+for i, text in enumerate(["Prillid"]):
+    radio = Radiobutton(button_frame, text=text, command=prillid)
+    radio.grid(row=5, column=2, padx=10, pady=10)
+    radios.append(radio)
+
+
+
 
 root.mainloop()
